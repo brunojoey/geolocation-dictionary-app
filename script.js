a442821cf41c47ee6fdf69bada9a7677c32e6baa
Joseph Bruno <brunojoey12@gmail.com>
2020-03-05 14:03:49 -0600

    added semicolons to the variables at top.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+var locationData = {
+  state: 'state',
+  city: 'city',
+  postalCode: 'postalCode',
+  word: []
+};
 
-var baseURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
-const apiKey = "?key=dea56771-7642-4f50-895c-bb6a6a34f4de";
-var searchTerm = "";
+var baseURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
+const apiKey = "?key=dea56771-7642-4f50-895c-bb6a6a34f4de"
+var searchTerm = ""
+var isFirstSearch = true;
 var url = baseURL + "default" + apiKey
 
 function sendQuery(url){
@@ -9,31 +16,13 @@ function sendQuery(url){
         url: url,
         method: "GET"
       }).then(function (response) {
+
         console.log(response);
         displayResponse(response);
     
     })
 }
 
-
-
-const geoApiKey = 'at_hD6JpWnRqX4YG6LsBbHUWzg0rBYAs'
-var geoURL = 'https://geo.ipify.org/api/v1?apiKey=' + geoApiKey
-var state;
-var city;
-var lat;
-var lng;
-
-$.ajax({
-    url: geoURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    state = response.location.region;
-    city = response.location.city;
-    lat = response.location.lat;
-    lng = response.location.lng;
-})
 
 function displayResponse(data){
 
@@ -56,8 +45,29 @@ function displayResponse(data){
 
 $("#search").on("click", function(){
     event.preventDefault();
-    console.log("it works");
-    searchTerm = $("#input").val();
-    url = baseURL+searchTerm+apiKey;
+    searchTerm = $("#input").val().toLowerCase();
+    url = encodeURI(baseURL + searchTerm + apiKey);
     sendQuery(url);
+    locationData.word.push(searchTerm);
+    localStorage.setItem('location', JSON.stringify(locationData));
+})
+
+
+
+
+const geoApiKey = 'at_hD6JpWnRqX4YG6LsBbHUWzg0rBYAs'
+var geoURL = 'https://geo.ipify.org/api/v1?apiKey=' + geoApiKey
+
+
+$.ajax({
+    url: geoURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+
+
+    locationData.state = response.location.region;
+    locationData.city = response.location.city;
+    locationData.postalCode = response.location.postalCode;
+    
 })
