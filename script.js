a2a1ae2f2e1f2768b760b56237f14b8c6ace84f1
Joseph Bruno <brunojoey12@gmail.com>
2020-03-05 12:01:16 -0600

    Added changes for master onto my branch again.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-var baseURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
-const apiKey = "?key=dea56771-7642-4f50-895c-bb6a6a34f4de"
-var searchTerm = ""
-var url = baseURL + "frozen" + apiKey
+var baseURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
+const apiKey = "?key=dea56771-7642-4f50-895c-bb6a6a34f4de";
+var searchTerm = "";
+var url = baseURL + "default" + apiKey
 
-$.ajax({
-    url: url,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
+function sendQuery(url){
+    $.ajax({
+        url: url,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+        displayResponse(response);
     
-})
+    })
+}
+
+
 
 const geoApiKey = 'at_hD6JpWnRqX4YG6LsBbHUWzg0rBYAs'
 var geoURL = 'https://geo.ipify.org/api/v1?apiKey=' + geoApiKey
@@ -28,4 +33,12 @@ $.ajax({
     city = response.location.city;
     lat = response.location.lat;
     lng = response.location.lng;
+})
+
+$("#search").on("click", function(){
+    event.preventDefault();
+    console.log("it works");
+    searchTerm = $("#input").val();
+    url = baseURL+searchTerm+apiKey;
+    sendQuery(url);
 })
